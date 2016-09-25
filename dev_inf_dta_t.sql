8614d91e0890bfe29317ea7bea18d87c413a178d
wdeqin <wdeqin@gmail.com>
2016-09-25 23:10:25 +0800

    add tern vim

--- dev_inf_dta_t.sql
+++ dev_inf_dta_t.sql
@@ -11,3 +11,26 @@ create table dev_inf_dta_t (
     primary key(infnam, fedseq)
 );
 
+
+insert into dev_inf_dta_t
+    (infnam, fedseq, fednam, fedtyp, fedlen, feddcm)
+    values
+    ('FDBRWPRFX1', 1, 'XINTVER', 'A', 8, 0)
+   ,('FDBRWPRFX1', 2, 'XSAACOD', 'A', 3, 0)
+   ,('FDBRWPRFX1', 3, 'XFNDCOD', 'A', 8, 0)
+   ,('FDBRWPRFX1', 4, 'XPRFDAT', 'S', 8, 0)
+   ,('FDBRWPRFX1', 5, 'XSPC080', 'O', 80, 0)
+   
+   ,('FDBRWPRFZ1', 1, 'XINTVER', 'A', 8, 0)
+   ,('FDBRWPRFZ1', 2, 'XSAACOD', 'A', 3, 0)
+   ,('FDBRWPRFZ1', 3, 'XFNDCOD', 'A', 8, 0)
+   ,('FDBRWPRFZ1', 4, 'XPRFDAT', 'S', 8, 0)
+   ,('FDBRWPRFZ1', 5, 'XSPC080', 'O', 80, 0)
+
+   ,('FDBRWPRFY1', 1, 'XINTVER', 'A', 8, 0)
+   ,('FDBRWPRFY1', 2, 'XSAACOD', 'A', 3, 0)
+   ,('FDBRWPRFY1', 3, 'XFNDCOD', 'A', 8, 0)
+   ,('FDBRWPRFY1', 4, 'XPRFDAT', 'S', 8, 0)
+   ,('FDBRWPRFY1', 5, 'XSPC080', 'O', 80, 0)
+
+   ;
